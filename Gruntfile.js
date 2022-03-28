41f783a0564578b4228178cda46891e6c8133bd6
S1eepingdog <yuanqi.dan@hotmail.com>
2022-03-27 21:53:44 -0400

    use grunt as pkg tool

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,10 +10,46 @@ module.exports = function (grunt) {
                 src: 'src/index.js',
                 dest: 'build/js/<%=pkg.name%>-<%=pkg.version%>.min.js'
             }
+        },
+        //css压缩
+        cssmin: {
+            options: {
+                report: 'gzip'
+            },
+            build: {
+                expand: true,
+                cwd: 'src/css',
+                src: ['index.css'],
+                dest: 'build/static/css'
+            }
+        },
+        htmlmin: {
+            options: {
+                removeComments: true,
+                removeCommentsFromCDATA: true,
+                collapseWhitespace: true,
+                collapseBooleanAttributes: true,
+                removeAttributeQuotes: true,
+                removeRedundantAttributes: true,
+                useShortDoctype: true,
+                removeEmptyAttributes: true,
+                removeOptionalTags: true
+            },
+            build: {
+                expand: true,
+                cwd: 'src',
+                src: ['*.html'],
+                dest: 'build'
+            }
         }
     });
-    // 加载提供"uglify"任务的插件
-    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-uglify'); //加载完成任务所需要的模块
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-htmlmin');
     // 默认任务
-    grunt.registerTask('default', ['uglify']);
+    grunt.registerTask('default', [
+        'uglify',
+        'cssmin',
+        'htmlmin'
+    ]); //定义任务
 }
